ca9eaaf17e13d895ef9e3907fdec06ab9dbcab9f
Shahid Qureshi <qureshi.shahid7777@gmail.com>
2022-07-24 15:17:53 +0530

    .env file removed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // to work with expressjs
 const express = require("express");
-
+const path = require("path");
 // it will use for all secrects
 const dotenv = require("dotenv");
 // calling express function and store meta data inside app, now app has all property of express
@@ -22,9 +22,16 @@ app.use(express.json());
 app.use(cookieParser());
 require("./database/conn");
 
-if (process.env.NODE_ENV == "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
+
 app.use(require("./routes/route"));
 
 // initializing the server at port value mentioned in config.env file
